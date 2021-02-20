4cd0aa6696e7965fc2f6233ff9eeba22802e45ed
Iaroslav Postovalov <postovalovya@gmail.com>
2021-02-20 15:56:26 +0700

    Update automation scripts

--- .space.kts
+++ .space.kts
@@ -1,3 +1,12 @@
 job("Build") {
-    gradlew("openjdk:11", "-Dorg.gradle.daemon=false", "build")
+    container("openjdk:11.0.10-jdk-buster") {
+        shellScript {
+            interpreter = "/bin/bash"
+
+            content = """
+                apt install libncurses5
+                ./gradlew -Dorg.gradle.daemon=false build
+            """.trimIndent()
+        }
+    }
 }
