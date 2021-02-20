a2c04455479e8939e91731c6843c765a9c3e1037
Iaroslav Postovalov <postovalovya@gmail.com>
2021-02-20 15:22:40 +0700

    Remove incorrect test

--- src/nativeTest/kotlin/ErrorsHandlingTest.kt
+++ src/nativeTest/kotlin/ErrorsHandlingTest.kt
@@ -6,14 +6,6 @@ import kotlin.test.assertFailsWith
 
 internal class ErrorsHandlingTest {
     @Test
-    fun blockAllocation() {
-        assertFailsWith<GslException> {
-            ensureHasGslErrorHandler()
-            gsl_block_calloc(ULong.MAX_VALUE)
-        }
-    }
-
-    @Test
     fun matrixAllocation() {
         assertFailsWith<GslException> {
             GslRealMatrixContext { produce(Int.MAX_VALUE, Int.MAX_VALUE) { _, _ -> 0.0 } }
