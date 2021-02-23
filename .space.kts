d8e9f869209fb6c60660da273fd9d7a68e67ab49
Iaroslav Postovalov <postovalovya@gmail.com>
2021-02-23 15:15:41 +0700

    Update build image

--- .space.kts
+++ .space.kts
@@ -1,13 +1,8 @@
 job("Build") {
-    container("mipt-npm.registry.jetbrains.space/p/sci/containers/ci-environment:1.0.0") {
+    container("mipt-npm.registry.jetbrains.space/p/sci/containers/ci-environment:1.0.1") {
         shellScript {
             interpreter = "/bin/bash"
-
-            content = """
-                apt update
-                apt install -y --no-install-recommends libncurses5
-                ./gradlew -Dorg.gradle.daemon=false build
-            """.trimIndent()
+            content = "./gradlew -Dorg.gradle.daemon=false build"
         }
     }
 }
