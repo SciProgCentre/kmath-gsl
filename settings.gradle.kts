a20c20e192a437f3293c74c8aae6876b4b5309b9
Iaroslav Postovalov <postovalovya@gmail.com>
2021-07-16 18:16:57 +0700

    Cleanup settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,8 @@ pluginManagement {
         maven("https://repo.kotlin.link")
     }
 
-    val toolsVersion = "0.10.0"
-
     plugins {
-        id("ru.mipt.npm.gradle.project") version toolsVersion
-        id("ru.mipt.npm.gradle.common") version toolsVersion
+        id("ru.mipt.npm.gradle.project") version "0.10.0"
         id("de.undercouch.download") version "4.1.2"
         kotlin("multiplatform") version "1.5.21"
     }
