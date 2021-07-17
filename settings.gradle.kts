79042abf5a5b7550871010073d2a567ac510666b
Iaroslav Postovalov <postovalovya@gmail.com>
2021-07-18 05:44:31 +0700

    Use correct HMPP configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://repo.kotlin.link")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     }
 
     plugins {
         id("ru.mipt.npm.gradle.project") version "0.10.2-fixrelease-1"
         id("de.undercouch.download") version "4.1.2"
-        kotlin("multiplatform") version "1.5.21"
+        kotlin("multiplatform") version "1.5.30-RC-161"
     }
 }
 
